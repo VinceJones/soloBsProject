b1232f1dba3bcb33210400161d08fdbee9558845
Vince Jones <vincethebutcher@gmail.com>
2015-04-28 12:06:28 -0500

    Initial Commit

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-var apikey = ''; // Put your API key here
-
+var apikey = '10d0a85137d94904c75e456dcdf91468227c09ca'; // Put your API key here
+var j;
+var k;
+var i;
 
 // Use this function to do stuff with your results. 
 // It is called after 'search' is executed.
 function searchCallback(results) {
     console.log(results);
+    displayContent(results);
 }
 
+function displayContent(results) {
+
+	for (j = 0; j < 9; j++){
+		$(".content").append("<div class='col-md-3 resultContent'><img src='" + results[j].image.medium_url +"'/><p class='lead'>Name: " + results[j].name + "</p><p>Description: " + results[j].deck +"</p><button class='minify'>Minify</button></div>");
+		console.log(results[j]);
+	}
+}
+
+
 $(document).ready(function() {
+
+	$(".content").append("Please wait....")
+	search("batman");
+
 
 	// Start the search here!
 	
